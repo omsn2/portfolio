2354a3168f26bc68d9b3bd0c4de103b7b734b737
omsn2 <138763819+omsn2@users.noreply.github.com>
2025-04-14 12:11:30 +0530

    certificate path changes

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -111,13 +111,13 @@ const Certifications = () => {
       <SectionTitle>Certifications</SectionTitle>
       <CertificationItem {...fadeInUp} {...hoverEffect}>
         <h3>Microsoft Certified Azure AI Fundamentals - KodeKloud</h3>
-        <CertificateButton onClick={() => openModal('/assets/certificates/Azure_page-0001.jpg')}>
+        <CertificateButton onClick={() => openModal('./public/assets/certificates/Azure_page-0001.jpg')}>
   Show Certificate
 </CertificateButton>
       </CertificationItem>
       <CertificationItem {...fadeInUp} {...hoverEffect}>
         <h3>Docker Training Course for the Absolute Beginner - KodeKloud</h3>
-        <CertificateButton onClick={() => openModal('/assets/certificates/Docker_page-0001.jpg')}>
+        <CertificateButton onClick={() => openModal('./public/assets/certificates/Docker_page-0001.jpg')}>
   Show Certificate
 </CertificateButton>
       </CertificationItem>
